3f4a5c31cf0ce593e829986a5c46556a5e6f9bf8
jaidenbm0127 <64822674+jaidenbm0127@users.noreply.github.com>
2024-07-21 20:42:19 -0400

    Implemented click event for recipe paper

---
+++ Scripts/Models/LanternScript.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+@onready var input_manager = $"../InputManager"
+
+func _ready():
+	input_manager.ObjectClicked.connect(_on_ObjectClicked)
+
+func _on_ObjectClicked(event_position, clicked_object):
+	if clicked_object.is_in_group("lantern"):
+		_handle_lantern_clicked()
+
+func _handle_lantern_clicked() -> void:
+	print("Lantern clicked!")
---
+++ Scripts/Models/PaperScript.gd
@@ -0,0 +1,47 @@
+extends Node3D
+
+@onready var input_manager = $"../InputManager"
+@onready var camera = $"../Camera3D"
+@onready var is_in_front: bool = false
+
+@export var animation_time = .5
+
+var original_position: Vector3
+var original_rotation: Vector3
+var camera_offset: Vector3
+
+func _ready():
+	# Initialize the original position and rotation
+	original_position = position
+	original_rotation = rotation_degrees
+	
+	# Calculate the camera offset
+	var camera_position = camera.global_transform.origin
+	camera_offset = Vector3(camera_position.x,
+							camera_position.y - 1.25, 
+							camera_position.z - 3.25)
+	input_manager.ObjectClicked.connect(_on_ObjectClicked)
+
+func _on_ObjectClicked(_event_position, clicked_object):
+	if clicked_object.is_in_group("paper"):
+		_handle_paper_clicked()
+
+func _handle_paper_clicked() -> void:
+	var tween = get_tree().create_tween().set_parallel(true)
+	if is_in_front:
+		# Return to original position and rotation
+		tween.tween_property(self, 
+							"position", 
+							original_position, 
+							animation_time)
+		tween.tween_property(self, 
+							"rotation_degrees", 
+							original_rotation, 
+							animation_time)
+	else:
+		# Move and rotate the paper to be in front of the camera
+		tween.tween_property(self, "position", camera_offset, animation_time)
+		tween.tween_property(self, "rotation_degrees", Vector3(0, -90, -75), animation_time)
+	
+	# Toggle the state
+	is_in_front = !is_in_front
