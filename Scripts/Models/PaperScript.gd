35892f1663578083fe3e9641cb05f39df50f451f
jaidenbm0127 <64822674+jaidenbm0127@users.noreply.github.com>
2024-07-21 22:46:47 -0400

    Added a flicker and turn off/on effect to lantern

--- Scripts/Models/PaperScript.gd
+++ Scripts/Models/PaperScript.gd
@@ -3,7 +3,7 @@ extends Node3D
 @onready var input_manager = $"../InputManager"
 @onready var camera = $"../Camera3D"
 @onready var is_in_front: bool = false
-
+@onready var light: OmniLight3D = null
 @export var animation_time = .5
 
 var original_position: Vector3
@@ -12,6 +12,9 @@ var camera_offset: Vector3
 
 func _ready():
 	# Initialize the original position and rotation
+	var lantern = get_node("../Lantern")
+	light = lantern.find_child("LanternLight", true, false)
+
 	original_position = position
 	original_rotation = rotation_degrees
 	
@@ -27,8 +30,9 @@ func _on_ObjectClicked(_event_position, clicked_object):
 		_handle_paper_clicked()
 
 func _handle_paper_clicked() -> void:
-	var tween = get_tree().create_tween().set_parallel(true)
 	if is_in_front:
+		var tween = get_tree().create_tween().set_parallel(true)
+		
 		# Return to original position and rotation
 		tween.tween_property(self, 
 							"position", 
@@ -40,8 +44,10 @@ func _handle_paper_clicked() -> void:
 							animation_time)
 	else:
 		# Move and rotate the paper to be in front of the camera
-		tween.tween_property(self, "position", camera_offset, animation_time)
-		tween.tween_property(self, "rotation_degrees", Vector3(0, -90, -75), animation_time)
+		if light.light_energy > 0:
+			var tween = get_tree().create_tween().set_parallel(true)
+			tween.tween_property(self, "position", camera_offset, animation_time)
+			tween.tween_property(self, "rotation_degrees", Vector3(0, -90, -75), animation_time)
 	
 	# Toggle the state
 	is_in_front = !is_in_front
