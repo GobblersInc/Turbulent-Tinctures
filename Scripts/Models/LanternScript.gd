bcd01008a3a7b05d77616f542e2a88d2e0fe9a1a
jaidenbm0127 <64822674+jaidenbm0127@users.noreply.github.com>
2024-07-23 14:06:11 -0400

    Reworked lantern logic

--- Scripts/Models/LanternScript.gd
+++ Scripts/Models/LanternScript.gd
@@ -1,55 +1,51 @@
 extends Node3D
 
-@onready var light : OmniLight3D = $Lantern/LanternLight
+@onready var light: OmniLight3D = $Lantern/LanternLight
 @onready var rng = RandomNumberGenerator.new()
-@onready var check_timer : Timer = $Lantern/CheckTimer
-@onready var lights_out_timer : Timer = $Lantern/LightsOutTimer
-@onready var lights_out_cooldown_timer : Timer = $Lantern/LightsOutCooldownTimer
+@onready var timer: Timer = $Lantern/Timer
 
 @export var flicker_probability: float
 @export var light_out_duration: float
 @export var check_interval: float
 @export var lights_out_cooldown: float
+@export var light_on_or_off: bool
+
+var is_light_on: bool
 
 func _ready():
-	check_timer.wait_time = check_interval
-	lights_out_timer.wait_time = light_out_duration
-	lights_out_cooldown_timer.wait_time = lights_out_cooldown
-	
-	check_timer.connect("timeout", _on_check_timer_timeout)
-	lights_out_timer.connect("timeout", _on_light_out_timer_timeout)
-	
-	check_timer.start()
-	
-func _on_check_timer_timeout():
-	print("We're in check timer timout")
-	# Check the probability and trigger the effect if conditions are met
-	if should_flicker_and_fade():
+	timer.connect("timeout", _on_check_timer_timeout)
+	is_light_on = light_on_or_off
+	if is_light_on:
+		timer.start(check_interval)
+	else:
 		flicker_and_fade_light()
 
-func _on_light_out_timer_timeout():
-	# Turn the light back on and restart the check timer
+func _on_check_timer_timeout():
+	if is_light_on:
+		if should_flicker_and_fade():
+			flicker_and_fade_light()
+		else:
+			timer.start(check_interval)
+	else:
+		turn_light_on()
+
+func turn_light_on():
 	var tween = get_tree().create_tween()
 	tween.tween_property(light, "light_energy", 6, 1)
-	lights_out_cooldown_timer.start()
+	is_light_on = true
+	timer.start(lights_out_cooldown)
 
 func should_flicker_and_fade() -> bool:
-	# Return true if the random value is less than the flicker probability
-	if lights_out_cooldown_timer.is_stopped():
-		return rng.randf() < flicker_probability
-	return false
+	return rng.randf() < flicker_probability
 
 func flicker_and_fade_light():
-	# Define the flicker durations and intensities
 	var tween = get_tree().create_tween()
-	var flicker_durations = [0.05, 0.1, 0.1, 0.25, 0.05]  # in seconds
+	var flicker_durations = [0.05, 0.1, 0.1, 0.25, 0.05]
 	var flicker_intensities = [7.0, 3, 5, 1, 2]
 
-	# Chain the flicker animations
 	for i in range(flicker_durations.size()):
 		tween.tween_property(light, "light_energy", flicker_intensities[i], flicker_durations[i])
 
-	# After flickering, fade out the light completely
-	tween.tween_property(light, "light_energy", 0.0, .4)  # 2 seconds to fade out
-	
-	lights_out_timer.start()
+	tween.tween_property(light, "light_energy", 0.0, 0.4)
+	is_light_on = false
+	timer.start(light_out_duration)
